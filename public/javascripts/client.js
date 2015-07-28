8d9924ef2b66d7a38d70d578b95598902d83697a
thekthuser <thekthuser@thekthuser.com>
2015-07-28 17:03:57 -0400

    backbone view, template. cannot pass variables

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
         url: '/items/',
     });
 
-
     /*
     //var list = new ItemCollection();
     //console.log(list.fetch());
@@ -29,5 +28,26 @@ $(document).ready(function() {
         }
     });
     */
+
+    var View = Backbone.View.extend({
+        el: '#container',
+        render: function() {
+            var list = new ItemCollection();
+            var badScopeEl = this.$el; //cannot access this.$el from inside functions
+            list.fetch({
+                success: function(items) {
+                    console.log(items.models);
+                    _.each(items.models, function(item) {
+                        //badScopeEl.append(item.attributes.note);
+                        var a = _.template( $('#template').html(), {note: item.attributes.note});
+                        badScopeEl.append(a);
+                    });
+                },
+            });
+        },
+    });
+
+    var view = new View();
+    view.render();
 
 });
