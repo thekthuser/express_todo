ab19804693004bba67dc9c2551130f1c92ddd5d7
thekthuser <thekthuser@thekthuser.com>
2015-08-06 20:40:32 -0400

    display username on index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var currentUser = Parse.User.current();
+    if (currentUser) {
+        res.render('index', {title: 'Express', user: currentUser.getUsername()});
+    } else {
+        res.render('index', {title: 'Express', user: 'Nobody'});
+    }
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
